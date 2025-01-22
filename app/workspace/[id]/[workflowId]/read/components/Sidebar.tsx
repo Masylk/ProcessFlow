8ee5087c9fcadab05b5e4ffb39341ab5446a2e90
Masylk <togbemaxime@gmail.com>
2025-01-22 11:21:42 +0100

    integrated logo design

--- app/workspace/[id]/[workflowId]/read/components/Sidebar.tsx
+++ app/workspace/[id]/[workflowId]/read/components/Sidebar.tsx
@@ -12,22 +12,17 @@ const Sidebar: React.FC<SidebarProps> = ({ stepCount }) => {
         <div className="h-[72px] flex-col justify-center items-start gap-6 inline-flex">
           <div className="self-stretch h-8 pl-6 pr-5 flex-col justify-start items-start flex">
             <div className="w-[142px] justify-start items-start inline-flex">
-              <div className="justify-end items-center gap-3 flex">
+              <div className="justify-end font-['Inter'] items-center text-xl font-bold gap-3 flex">
                 <div className="shadow-[0px_1px_2px_0px_rgba(16,24,40,0.06)] justify-start items-start inline-flex">
-                  <div className="w-8 h-8 relative bg-gradient-to-bl from-[#4d6de3] to-[#1b2860] rounded-lg shadow-[inset_0px_0px_0px_0.6666666865348816px_rgba(16,24,40,0.24)] border border-white overflow-hidden">
-                    <div className="w-8 h-8 left-0 top-0 absolute bg-gradient-to-b from-white to-white" />
-                    <div className="w-5 h-6 left-[6px] top-[4px] absolute shadow-[0px_1.5px_3px_-1px_rgba(36,36,36,0.10)]">
-                      <div className="w-3.5 h-[21.28px] left-[3.33px] top-[1.33px] absolute">
-                        {/* Image added here */}
-                        <img
-                          src="https://grzxiilmiwwwepaymqnu.supabase.co/storage/v1/object/public/public-assets/assets/shared_components/pf_logo.svg"
-                          alt="Logo"
-                          className="w-full h-full object-contain"
-                        />
-                      </div>
-                    </div>
+                  <div className="w-8 h-8 relative bg-gradient-to-bl from-[#4d6de3] to-[#1b2860] rounded-lg shadow-[inset_0px_0px_0px_0.6666666865348816px_rgba(16,24,40,0.24)] overflow-hidden">
+                    <img
+                      src="https://grzxiilmiwwwepaymqnu.supabase.co/storage/v1/object/public/public-assets/assets/shared_components/processflow_logo.png"
+                      alt="Logo"
+                      className="w-full h-full object-contain"
+                    />
                   </div>
                 </div>
+                Processflow
               </div>
             </div>
           </div>
