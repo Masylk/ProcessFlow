ddc963637a5a0c803594abc63963d6b4922d4692
Masylk <togbemaxime@gmail.com>
2025-02-02 11:20:11 +0100

    validate the pr

--- test.js
+++ test.js
@@ -2,14 +2,15 @@ const assert = require('assert');
 
 // Fonction à tester
 function addition(a, b) {
-    return a + b;
+  return a + b;
 }
 
 // Test
 try {
-    assert.strictEqual(addition(2, 3), 5);
-    console.log('Test passé : addition(2, 3) retourne 5');
+  assert.strictEqual(addition(2, 3), 5);
+  console.log('Test passé : addition(2, 3) retourne 5');
 } catch (error) {
-    console.error('Test échoué :', error.message);
-} a
-aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+  console.error('Test échoué :', error.message);
+}
+a;
+aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa;
