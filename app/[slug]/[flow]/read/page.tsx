2ea49263e15d7442556bbd31cff023e0958f3539
Masylk <togbemaxime@gmail.com>
2025-04-17 11:17:44 +0200

    fix build errors

--- app/[slug]/[flow]/read/page.tsx
+++ app/[slug]/[flow]/read/page.tsx
@@ -1,8 +1,14 @@
+import { redirect } from 'next/navigation';
 import { Metadata } from 'next';
 import ReadPageClient from './components/ReadPageClient';
 
 interface PageParams {
+  slug: string;
   flow: string;
+}
+
+interface PageProps {
+  params: Promise<PageParams>;
 }
 
 interface Workflow {
@@ -18,14 +24,14 @@ interface Workflow {
   updated_at: string;
   created_at: string;
 }
+
 export async function generateMetadata({
   params,
 }: {
   params: Promise<PageParams>;
 }): Promise<Metadata> {
-  const resolvedParams = await params;
-
   // Decode the flow parameter
+  const resolvedParams = await params;
   const [workflowName, workflowId] = resolvedParams.flow.split('--pf-');
   if (!workflowName || !workflowId) {
     return { title: 'ProcessFlow' };
@@ -46,10 +52,31 @@ export async function generateMetadata({
   };
 }
 
-export default function Page({
-  params,
-}: {
-  params: { slug: string; flow: string };
-}) {
+export default async function ReadPage(props: PageProps) {
+  // Decode the flow parameter
+  const params = await props.params;
+  const [workflowName, workflowId] = params.flow.split('--pf-');
+  if (!workflowName || !workflowId) {
+    redirect('/'); // Redirect to root if parameters are invalid
+  }
+
+  // Get workflow data from API using path parameter
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_APP_URL}/api/workflows/${workflowId}`
+  );
+
+  if (!response.ok) {
+    // Handle unauthorized or not found cases
+    const { error } = await response.json();
+    if (response.status === 401) {
+      redirect('/login');
+    } else {
+      redirect('/');
+    }
+  }
+
+  // You can pass workflow props here if needed
+  // const workflow: Workflow = await response.json();
+
   return <ReadPageClient />;
 }
