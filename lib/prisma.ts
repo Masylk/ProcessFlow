83affb3c2a05417b5f54443d699b881a23ead19f
Masylk <togbemaxime@gmail.com>
2025-05-27 08:57:57 +0200

    remove logs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -8,13 +8,11 @@ let prisma: PrismaClient;
 
 // In production, don't use the global object to prevent memory leaks
 if (process.env.NODE_ENV === 'production') {
-  console.log('creating new prisma client in production');
   prisma = new PrismaClient();
 } else {
   // In non-production, use global to avoid multiple instances during hot-reloading
   if (!globalForPrisma.prisma) {
     globalForPrisma.prisma = new PrismaClient();
-    console.log('creating new prisma client in non-production');
   }
   prisma = globalForPrisma.prisma;
 }
