02432a87fe1e2ec7fdaa50df325698fe4b1c5bdb
Masylk <togbemaxime@gmail.com>
2025-05-28 08:06:28 +0200

    add logs

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -9,15 +9,15 @@ let prisma: PrismaClient | undefined;
 
 // If running on Vercel, do not create a Prisma client
 if (process.env.VERCEL) {
-  // console.log('Running on Vercel, not creating Prisma client');
+  console.log('Running on Vercel, not creating Prisma client');
   prisma = undefined;
 } else if (process.env.NODE_ENV === 'production') {
-  // console.log('Running in production, creating Prisma client');
+  console.log('Running in production, creating Prisma client');
   prisma = new PrismaClient();
 } else {
   // In non-production, use global to avoid multiple instances during hot-reloading
   if (!globalForPrisma.prisma) {
-    // console.log('Creating Prisma client in non-production environment');
+    console.log('Creating Prisma client in non-production environment');
     globalForPrisma.prisma = new PrismaClient();
   }
   prisma = globalForPrisma.prisma;
